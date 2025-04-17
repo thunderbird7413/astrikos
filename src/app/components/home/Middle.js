63e8682fec1bf9d8c92325196cbb91229c6c39cf
piyush <pmimpiyush@gmail.com>
2025-04-17 23:37:10 +0530

    minor changes on home

--- src/app/components/home/Middle.js
+++ src/app/components/home/Middle.js
@@ -6,27 +6,32 @@ function Middle() {
     
     <div className="middle ">
      
-      <h2 className="title">Unlimited Possibility,<br />Unlimited Collaboration</h2>
+     <div>
+      <div className='h-[50%]'></div>
+      <h2 className="title">Visualize Smarter, <br />Collaborate Better</h2>
       <ul className="features">
         <li className="feature">
-          <span className="plus-icon">+</span> Instant Feedback
+          <span className="plus-icon">+</span>  Real-Time 3D & 2D Editing
         </li>
         <li className="feature">
-          <span className="plus-icon">+</span> Enhanced Creativity
+          <span className="plus-icon">+</span> Collaborative Map Visualization
         </li>
         <li className="feature">
-          <span className="plus-icon">+</span> Efficient Workflows
+          <span className="plus-icon">+</span> Smooth File Imports (GLTF, GeoJSON & more)
         </li>
         <li className="feature">
-          <span className="plus-icon">+</span> Simultaneous Editing
+          <span className="plus-icon">+</span> Instant Previews & Feedback
+
         </li>
         <li className="feature">
-          <span className="plus-icon">+</span> Global Accessibility
+          <span className="plus-icon">+</span> Cross-Team Collaboration
+
         </li>
         <li className="feature">
-          <span className="plus-icon">+</span> Seamless Communication
+          <span className="plus-icon">+</span> Zero Setup, Fully Web-Based
         </li>
       </ul>
+      </div>
     </div>
   );
 }
